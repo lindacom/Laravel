4c42b420275955c6daa2831058ab056662e82d74
lindacom <43495865+lindacom@users.noreply.github.com>
2019-08-19 21:20:55 +0100

    Create studInsertController.php

---
+++ User management/add a user/studInsertController.php
@@ -0,0 +1,19 @@
+<?php
+namespace App\Http\Controllers;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+
+class StudInsertController extends Controller {
+   public function insertform() {
+      return view('stud_create');
+   }
+	
+   public function insert(Request $request) {
+      $name = $request->input('stud_name');
+      DB::insert('insert into users (name) value (?)', [$name]);
+      echo "Record inserted successfully.<br/>";
+      
+   }
+}
