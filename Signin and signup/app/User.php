750cfb1b43d8c47119cb657d3d943cf66dfc37c8
lindacom <43495865+lindacom@users.noreply.github.com>
2019-08-24 20:54:06 +0100

    Create User.php

---
+++ Signin and signup/app/User.php
@@ -0,0 +1,9 @@
+<?php
+namespace App;
+use Illuminate\Contracts\Auth\Authenticatable;
+use Illuminate\Database\Eloquent\Model;
+class User extends Model implements Authenticatable
+{
+    use \Illuminate\Auth\Authenticatable;
+}
+
