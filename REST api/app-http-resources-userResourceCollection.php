5fadaae36410f56e93cb086302df40bd2dd9a47f
lindacom <43495865+lindacom@users.noreply.github.com>
2019-09-01 21:57:18 +0100

    Create app-http-resources-userResourceCollection.php

---
+++ REST api/app-http-resources-userResourceCollection.php
@@ -0,0 +1,20 @@
+<?php
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\ResourceCollection;
+
+class UserResourceCollection extends ResourceCollection
+
+{
+
+
+public function toArray($request)
+{
+    return parent::toArray($request);
+}
+
+
+
+
+
+}
