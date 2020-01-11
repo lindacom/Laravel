bcc4de2dbe156896a85e8f4db5f3aaf87e6f2506
lindacom <43495865+lindacom@users.noreply.github.com>
2020-01-11 20:31:57 +0000

    Create footer.blade.php

---
+++ magazine style template/footer.blade.php
@@ -0,0 +1,19 @@
+<!-- footer -->
+
+<footer">
+  <div class="container">
+  <div>
+    <p class="float-right">
+      
+    </p>
+    <p></p>
+    <p></p>
+  </div></div>
+  </footer>
+
+  <!-- copyright -->
+  <div>
+  <div>
+  <p>copyright</p>
+  <a></a></div></div>
+
