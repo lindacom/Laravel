1174a94dde8abf9308bbec5a48dbc010ac61c4ed
lindacom <43495865+lindacom@users.noreply.github.com>
2020-07-24 20:41:54 +0100

    Create post.blade.php

---
+++ posts/post.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+     <body>
+          
+              
+                    Laravel Posts
+
+                 <p>  {{ $post->body }} </p>
+               
+
+               
+                
+           
+            
+        
+    </body>
+</html>
