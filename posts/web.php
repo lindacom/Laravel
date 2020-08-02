1654d6999bdeee306c4e49889a0c04c39e0c6350
lindacom <43495865+lindacom@users.noreply.github.com>
2020-08-02 12:08:03 +0100

    Update web.php

--- posts/web.php
+++ posts/web.php
@@ -2,6 +2,18 @@
 
 use Illuminate\Support\Facades\Route;
 
+Route::get('/', function () {
+     return view('welcome');
+});
 
-Route::get('/{slug}', 'PostsController@show');
+Route::get('/posts', 'PostsController@index');
+Route::post('/posts', 'PostsController@store');
+Route::get('/posts/create', 'PostsController@create');
+Route::get('/posts/{post}', 'PostsController@show');
+Route::get('/posts/{post}/edit', 'PostsController@edit');
+Route::put('/posts/{post}', 'PostsController@update');
+Route::get('/posts/{post}/delete', 'PostsController@delete');
+Route::delete('/posts/{post}/delete', 'PostsController@destroy');
+
+Auth::routes();
    
